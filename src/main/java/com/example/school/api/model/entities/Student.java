75fdea80947faa4d28da433bea5a0609e5a4a85d
Marcos de Souza Ribeiro Junior <marcossrj90@gmail.com>
2022-06-01 19:54:07 -0300

    Adding Student class and mapping with JPA

---
+++ src/main/java/com/example/school/api/model/entities/Student.java
@@ -0,0 +1,56 @@
+package com.example.school.api.model.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Student {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@NotBlank
+	private String name;
+	@Email
+	private String email;
+	
+	public Student() {
+	
+	}
+
+	public Student(String name, String email) {
+		super();
+		this.name = name;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
