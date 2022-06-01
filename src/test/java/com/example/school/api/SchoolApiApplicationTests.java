e57c72052f964a1889d6578d40489aeabd36dd62
Marcos de Souza Ribeiro Junior <marcossrj90@gmail.com>
2022-06-01 19:38:39 -0300

    Basic files from Spring Initializr

---
+++ src/test/java/com/example/school/api/SchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.school.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
